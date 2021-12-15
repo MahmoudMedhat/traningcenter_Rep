c5beea6646e0b52bafcbfdea0bfeb457d33befdc
Mohamed Agour <mmedhat@bebrand.tv>
2021-12-15 17:18:35 +0200

    first commit

---
+++ traningcenter/Controllers/instructorsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using traningcenter.Models;
+
+namespace traningcenter.Controllers
+{
+    public class instructorsController : Controller
+    {
+        private traningcenterEntities db = new traningcenterEntities();
+
+        // GET: instructors
+        public ActionResult Index()
+        {
+            var instructors = db.instructors.Include(i => i.cours);
+            return View(instructors.ToList());
+        }
+
+        // GET: instructors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            instructor instructor = db.instructors.Find(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instructor);
+        }
+
+        // GET: instructors/Create
+        public ActionResult Create()
+        {
+            ViewBag.courseId = new SelectList(db.courses, "Id", "Name");
+            return View();
+        }
+
+        // POST: instructors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,instructors,courseId,date")] instructor instructor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.instructors.Add(instructor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.courseId = new SelectList(db.courses, "Id", "Name", instructor.courseId);
+            return View(instructor);
+        }
+
+        // GET: instructors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            instructor instructor = db.instructors.Find(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.courseId = new SelectList(db.courses, "Id", "Name", instructor.courseId);
+            return View(instructor);
+        }
+
+        // POST: instructors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,instructors,courseId,date")] instructor instructor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(instructor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.courseId = new SelectList(db.courses, "Id", "Name", instructor.courseId);
+            return View(instructor);
+        }
+
+        // GET: instructors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            instructor instructor = db.instructors.Find(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instructor);
+        }
+
+        // POST: instructors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            instructor instructor = db.instructors.Find(id);
+            db.instructors.Remove(instructor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ traningcenter/WebForm1.aspx.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using traningcenter.Models;
+
+namespace traningcenter
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                DateTime? searchText = null;
+
+                if (Request.QueryString["searchText"] != null)
+                {
+                    searchText = DateTime.Parse(Request.QueryString["searchText"]);
+                }
+                ReportViewer1.ProcessingMode = ProcessingMode.Local;
+                ReportViewer1.LocalReport.ReportPath = Server.MapPath("~/Reports/Report1.rdlc");
+                traningcenterEntities entities = new traningcenterEntities();
+                var data = searchText == null ? entities.Instructors_View.ToList()
+                : entities.Instructors_View.Where(
+                    x => x.date.Value.Month==searchText.Value.Month
+                    && x.date.Value.Year ==searchText.Value.Year
+                     && x.date.Value.Day == searchText.Value.Day
+                    ).ToList();
+                ReportDataSource datasource = new ReportDataSource("DataSet1", data);
+
+                ReportViewer1.LocalReport.DataSources.Clear();
+                ReportViewer1.LocalReport.DataSources.Add(datasource);
+            }
+
+        }
+    }
+}
